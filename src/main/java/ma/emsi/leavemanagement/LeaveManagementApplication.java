1f7454d58ea8c369aa71e07cb9ac9c227cbaefd2
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2023-12-27 01:08:18 +0100

    update leave balance every new year

--- src/main/java/ma/emsi/leavemanagement/LeaveManagementApplication.java
+++ src/main/java/ma/emsi/leavemanagement/LeaveManagementApplication.java
@@ -2,11 +2,26 @@ package ma.emsi.leavemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class LeaveManagementApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LeaveManagementApplication.class, args);
+
+
 	}
+
+	//Test Scheduled annotation
+
+//	@Scheduled(fixedDelay = 1000L)
+//	public static void  scheduletask(){
+//		System.out.println("keep it up");
+//	}
+
+
 }
--- src/main/java/ma/emsi/leavemanagement/controllers/LeaveControllerImpl.java
+++ src/main/java/ma/emsi/leavemanagement/controllers/LeaveControllerImpl.java
@@ -3,12 +3,14 @@ package ma.emsi.leavemanagement.controllers;
 
 import lombok.AllArgsConstructor;
 import ma.emsi.leavemanagement.entities.Leave;
+import ma.emsi.leavemanagement.services.Impl.LeaveBalanceService;
 import ma.emsi.leavemanagement.services.LeaveService;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LeaveControllerImpl implements LeaveController {
 
     private final LeaveService leaveService;
+    private final LeaveBalanceService leaveBalanceService;
 
     @PostMapping("/{idPerson}")
     public ResponseEntity<Leave> submitLeaveRequest(@RequestBody Leave leave,@PathVariable Long idPerson){
@@ -38,5 +41,11 @@ public class LeaveControllerImpl implements LeaveController {
         Leave canceledLeave=leaveService.cancelLeave(idLeave);
         return new ResponseEntity<>(canceledLeave, HttpStatus.OK);
     }
-    
+
+    @PostMapping("/reset")
+    @PreAuthorize("hasRole('ADMIN')") // Example authorization
+    public ResponseEntity<?> resetLeaveBalancesManually() {
+        leaveBalanceService.updateLeaveBalanceEachNewYear();
+        return ResponseEntity.ok("Leave balances reset successfully");
+    }
 }
